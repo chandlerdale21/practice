aa8aac837ed97b3f14b6ab639abe4a8646860cb3
Chandler Dale <chandlerdale21@gmail.com>
2023-04-17 17:35:06 -0500

    Coding challenge for 2023-04-17.

---
+++ CC 2023-04-17/index.js
@@ -0,0 +1,13 @@
+// Description
+// We need a function that can transform a string into a number. What ways of achieving this do you know?
+
+
+//P: There won't be any non integers values that will convert? 
+//R: given a string conver it to number
+//E: '7' = 7 
+//P: create a function that takes the parameter and then just return the Number method to the output
+
+function convertNum (string){
+    return Number(string)
+}
+console.log(convertNum('7'))
